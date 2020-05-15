98396f84455a82a6bb43fa4f2decd26ed278bbe5
fabrizio <fabrizio@ubimate.com>
2020-05-15 19:48:03 +0200

    left/right/up/downSwipe pseudo events (e.g. ev_leftSwipe)

--- src/pageamp/core/Element.hx
+++ src/pageamp/core/Element.hx
@@ -361,7 +361,12 @@ class Element extends Node {
 				// contains script
 				//TODO: this only supports single expressions (no ';' separator)
 				var evname = name.substr(EVENT_PREFIX.length);
-				e.domAddEventHandler(evname, v.evGet);
+                if (evname.endsWith('Swipe')) {
+                    var type = evname.substring(0, evname.indexOf('Swipe'));
+                    addSwipeHandler(type, v.evGet);
+                } else {
+                    e.domAddEventHandler(evname, v.evGet);
+                }
 			}
 		} else if (name.startsWith(HANDLER_PREFIX)) {
 			v.unlink(); // non refreshed
@@ -400,7 +405,61 @@ class Element extends Node {
 		return Util.makeHyphenName(n.substr(off));
 	}
 
-	// =========================================================================
+    // =========================================================================
+    // swipe detection
+    // =========================================================================
+
+    // https://stackoverflow.com/a/30192291/12573599
+    function addSwipeHandler(type:String, cb:DomEvent->Void) {
+        var xDown:Int = null;
+        var yDown:Int = null;
+        function handleTouchStart(evt:Dynamic) {
+            xDown = evt.touches[0].clientX;
+            yDown = evt.touches[0].clientY;
+        };
+        function handleTouchMove(evt:Dynamic) {
+            if (xDown == null || yDown == null) {
+                return;
+            }
+            var xUp = evt.touches[0].clientX;
+            var yUp = evt.touches[0].clientY;
+            var xDiff = xDown - xUp;
+            var yDiff = yDown - yUp;
+
+            if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
+                if ( xDiff > 0 ) {
+                    /* left swipe */
+                    if (type == 'left') {
+                        cb(cast evt);
+                    }
+                } else {
+                    /* right swipe */
+                    if (type == 'right') {
+                        cb(cast evt);
+                    }
+                }
+            } else {
+                if ( yDiff > 0 ) {
+                    /* up swipe */
+                    if (type == 'up') {
+                        cb(cast evt);
+                    }
+                } else {
+                    /* down swipe */
+                    if (type == 'down') {
+                        cb(cast evt);
+                    }
+                }
+            }
+            /* reset values */
+            xDown = null;
+            yDown = null;
+        };
+        e.domAddEventHandler('touchstart', handleTouchStart);
+        e.domAddEventHandler('touchmove', handleTouchMove);
+    }
+
+    // =========================================================================
 	// INNERTEXT_PROP reflection
 	// =========================================================================
 
