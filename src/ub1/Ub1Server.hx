a78343c90c9b145a065c0e8921989f81fe5a8b9f
fabrizio <fabrizio@ubimate.com>
2021-06-08 02:03:52 +0200

    adapted for ub1-java

--- src/ub1/Ub1Server.hx
+++ src/ub1/Ub1Server.hx
@@ -11,29 +11,31 @@ import ub1.server.dom.HtmlText;
 using ub1.lib.DomTools;
 
 class Ub1Server {
+	public static inline var CLIENT_JS_PATHNAME = '/.ub1/client/ub1.js';
 	
 	public static function main() {}
 	
-	public static function load(root:String, uri:String, domain='localhost', addClient=true): Page {
+	public static function load(root:String, uri:String, domain='localhost',
+			addClient=true, clientUrlPrefix=''): Page {
 		var url = new Url(uri);
 		var doc = new Preprocessor(root).read(url.path);
 		var page = ServerLoader.loadRoot(doc);
 		page.context.refresh();
 		if (addClient) {
-			Ub1Server.addClient(page);
+			Ub1Server.addClient(page, clientUrlPrefix);
 		}
 		return page;
 	}
 
 	#if !debug inline #end
-	static function addClient(p:Page) {
+	static function addClient(p:Page, clientUrlPrefix:String) {
 		// page properties
 		var e = new HtmlElement(p.doc.domGetBody(), 'script', 0, 0, 0);
 		var s = Ub1Common.PAGE_PROPS_VAR + ' = ' + Json.stringify(p.getProps()) + ';';
 		new HtmlText(e, s, 0, 0, 0, false);
 		// client code
 		var e = p.doc.domCreateElement('script');
-		e.domSet('src', '/.ub1/client/ub1.js');
+		e.domSet('src', clientUrlPrefix + CLIENT_JS_PATHNAME);
 		p.doc.domGetBody().domAddChild(e);
 	}
 
